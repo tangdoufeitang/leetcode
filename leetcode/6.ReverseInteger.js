0d92dbea86fc1a0899d5b3bdd3910f434d467804
tangdoudeitang <tangdoufeitang@163.com>
2017-09-30 18:49:05 +0800

    leetcodeforjs

---
+++ leetcode/6.ReverseInteger.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+     let res = 0
+    while(x){
+   
+    res=res*10+x%10
+    
+    x=parseInt(x/10)
+    }
+    if(res>Math.pow(2,31)||res<Math.pow(-2,31)) {
+        return 0
+    }
+    return res
+};
