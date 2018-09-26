0b220480673e8e1efe26f6536a9a7a2f23285846
糖豆非糖 <tangdoufeitang@163.com>
2018-09-26 15:58:18 +0800

    Add 190 questions to python's solution

---
+++ Python/190.Reverse Bits.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+# class Solution:
+#     # @param n, an integer
+#     # @return an integer
+#     def reverseBits(self, n):
+#     	result = 0
+#     	for i in range(32):
+#     		result <<= 1
+#     		result |= n & 1
+#     		n >>= 1
+    		
+#     	return result
+if __name__ == '__main__':
+	print(0<<2)
+	# print(Solution().reverseBits(43261596))
