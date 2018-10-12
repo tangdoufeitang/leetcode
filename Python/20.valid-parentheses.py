3c0ba1e471c5d66410bf71c3eb776e72a41f8b05
糖豆非糖 <tangdoufeitang@163.com>
2018-10-12 15:25:32 +0800

    :sparkles: Add 020 questions to python's solution

---
+++ Python/20.valid-parentheses.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+# 20. Valid Parentheses
+
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+
+# Example 1:
+
+# Input: "()"
+# Output: true
+# Example 2:
+
+# Input: "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: "(]"
+# Output: false
+# Example 4:
+
+# Input: "([)]"
+# Output: false
+# Example 5:
+
+# Input: "{[]}"
+# Output: true
+
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack, lookup = [], {'}': '{', ')': '(', ']': '['}
+        for i in s:
+        	if i in lookup.values():
+        		stack.append(i)
+        	elif i in lookup.keys():
+        		if len(stack) == 0 or lookup[i] != stack.pop():
+        			return False
+        	else:
+        		return False
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+	print(Solution().isValid('{}{}()'))
